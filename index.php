0dee57df6e952174ab3360a464ce9d16b88820fe
asmakhatun2313 <asmakhatun2313.gmail.com>
2024-02-06 01:11:20 -0300

    first-commit

---
+++ index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login Form</title>
+    <link rel="stylesheet" href="style/form.css">
+    <script src="https://kit.fontawesome.com/cf578c723d.js" crossorigin="anonymous"></script>
+</head>
+<body>
+    <section>
+        <div class="form">
+            <form action="">
+                <h1>Login</h1>
+
+                <div class="input-box">
+                    <input type="email" name="" id="" placeholder="Enter Your Email">
+                    <i class="fa-solid fa-envelope"></i>
+                </div>
+                <div class="input-box">
+                   <input type="password" name="" id="" placeholder="Enter Your Password">
+                   <i class="fa-solid fa-lock"></i>
+                </div>
+                <div class="remember-me">
+                    <label for=""><input type="checkbox" name="" id="">Remember me</label>
+                    <a href="#">Forget Password</a>    
+                </div> <button type="submit" class="btn">Login</button>
+                <div class="register-link">
+                    <p>Don't have any account?</p>
+                    <a href="signup.php">Sign Up</a>
+                    
+
+                </div>
+            </form>
+        </div>
+     
+
+    </section>
+</body>
+</html>
---
+++ signup.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sign Up page</title>
+    <link rel="stylesheet" href="style/signup.css">
+</head>
+<body>
+    <div class="signup-box">
+        <h1>Sign Up</h1>
+        <form >
+            <p>First Name:</p>
+            <input type="text" name="" placeholder="First-Name">
+            <p>Last Name:</p>
+            <input type="text" name="" placeholder="Last-Name">
+            <p>User Name:</p>
+            <input type="text" name="" placeholder="Do Not Contain white space">
+            <p>Email:</p>
+            <input type="email" name="" placeholder="Enter your Email">
+            <p>Password</p>
+            <input type="password" name="" placeholder="Up to 8 Character">
+            <p>Recheck Password</p>
+            <input type="password" name="" placeholder="Enter your password"> <br>
+            <button type="submit">Signup</button>
+
+            
+            <div class="login">
+                <p>you have an account?</p>
+               <a href="index.php">Login Here</a> 
+            </div>
+
+
+              
+        </form>
+    </div>
+    
+</body>
+</html>
